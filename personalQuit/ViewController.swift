182b23f1bd0e7b7d3571c7f1dc965622b6b32eee
Serhi <werewolfilya@gmail.com>
2021-03-05 07:43:36 +0200

    Initial Commit

---
+++ personalQuit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  personalQuit
+//
+//  Created by  zholon on 05/03/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
